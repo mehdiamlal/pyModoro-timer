581b7e373baf0c06b153fa6e20a3b8ae087b4f1b
Mehdi Amlal <mehdi.amlal.business@gmail.com>
2022-08-18 21:13:14 +0200

    added pause functionality

--- src/main.py
+++ src/main.py
@@ -1,15 +1,21 @@
+from signal import pause
 from tkinter import *
 from tkmacosx import Button
 
 BG_COLOR = "#FFF2F2"
 FONT_STYLE = "Roboto Mono"
 FONT_COLOR = "#F47C7C"
-initial_minutes = 25
-initial_seconds = 0
+minutes = 25
+seconds = 0
 timer_on = False
+pause_on = False
 
 def countdown_mechanism(m, s):
     """Fucntion that implements the countdown."""
+    global minutes
+    global seconds
+    minutes = m
+    seconds = s
     if m >= 0 and s >= 0 and timer_on:
         if s < 10 and s > 0:
             timer.config(text=f"{m}:0{s}")
@@ -27,14 +33,37 @@ def countdown():
     global timer_on
     timer_on = True
     start_btn["state"] = DISABLED
-    countdown_mechanism(initial_minutes, initial_seconds)
+    countdown_mechanism(minutes, seconds)
         
 
 def reset():
+    """Resets the timer to its initial value"""
     global timer_on
+    global pause_on
+    global minutes
+    global seconds
     timer_on = False
+    pause_on = False
+    minutes = 25
+    seconds = 0
     start_btn["state"] = NORMAL
-    timer.config(text=f"{initial_minutes}:0{initial_seconds}")
+    pause_btn.config(text="PAUSE")
+    timer.config(text=f"{minutes}:0{seconds}")
+
+def pause():
+    """Pauses timer's countdown."""
+    global pause_on
+    global timer_on
+    if pause_on:
+        pause_on = False
+        timer_on = True
+        pause_btn.config(text="PUASE")
+        countdown_mechanism(minutes, seconds)
+
+    else:
+        pause_on = True
+        timer_on = False
+        pause_btn.config(text="RESUME")
 
 window = Tk()
 window.title("pyModoro Timer")
@@ -42,12 +71,16 @@ window.minsize(width=800, height=800)
 window.config(background=BG_COLOR, padx=100, pady=150)
 
 #timer
-timer = Label(text=f"{initial_minutes}:0{initial_seconds}", font=(FONT_STYLE, 60, "normal"), bg=BG_COLOR, fg=FONT_COLOR, pady=50)
+timer = Label(text=f"{minutes}:0{seconds}", font=(FONT_STYLE, 60, "normal"), bg=BG_COLOR, fg=FONT_COLOR, pady=50)
 timer.pack()
 
 #start button
 start_btn = Button(text="START", command=countdown, bg=FONT_COLOR, fg=BG_COLOR, font=(FONT_STYLE, 35, "bold"), borderless=1)
 start_btn.pack()
+
+#pause button
+pause_btn = Button(text="PAUSE", command=pause,bg=FONT_COLOR, fg=BG_COLOR, font=(FONT_STYLE, 35, "bold"), borderless=1)
+pause_btn.pack()
 
 #reset button
 reset_btn = Button(text="RESET", command=reset,bg=FONT_COLOR, fg=BG_COLOR, font=(FONT_STYLE, 35, "bold"), borderless=1)
